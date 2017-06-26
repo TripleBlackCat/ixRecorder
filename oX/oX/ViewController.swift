c9122723d88c1e377a0dac66437cfb571f6d5b6a
Kavilan Nair <kavilann@gmail.com>
2017-06-26 16:16:12 +0200

    My first button

---
+++ oX/oX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oX
+//
+//  Created by Kavilan Nair on 2017/06/26.
+//  Copyright © 2017 Kavilan Nair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
