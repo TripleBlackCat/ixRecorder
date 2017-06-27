e51e272d94c6cebf5de26a0141192a975baac8a6
Kavilan Nair <kavilann@gmail.com>
2017-06-27 21:56:22 +0200

    Added recording and playing functionality

--- oX/oX/ViewController.swift
+++ oX/oX/ViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
 
+    var audioPlayer: AVAudioPlayer?
+    var audioRecorder: AVAudioRecorder?
+    
+    
+    
+    
+    
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var stopButton: UIButton!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        stopButton.isEnabled = false
+        let fileMgr = FileManager.default
+        
+        let dirPaths = fileMgr.urls(for: .documentDirectory,
+                                    in: .userDomainMask)
+        
+        let soundFileURL = dirPaths[0].appendingPathComponent("sound.caf")
+        
+        let recordSettings =
+            [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+             AVEncoderBitRateKey: 16,
+             AVNumberOfChannelsKey: 2,
+             AVSampleRateKey: 44100.0] as [String : Any]
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(
+                AVAudioSessionCategoryPlayAndRecord)
+        } catch let error as NSError {
+            print("audioSession error: \(error.localizedDescription)")
+        }
+        
+        do {
+            try audioRecorder = AVAudioRecorder(url: soundFileURL,
+                                                settings: recordSettings as [String : AnyObject])
+            audioRecorder?.prepareToRecord()
+        } catch let error as NSError {
+            print("audioSession error: \(error.localizedDescription)")
+        }
+    
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +70,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startRecording(_ sender: UIButton) {
+        
+        print("The button has been pressed")
+    }
+    
+    
 
+    @IBAction func stopRecording(_ sender: Any) {
+        
+        stopButton.isEnabled = false
+        recordButton.isEnabled = true
+        
+        if audioRecorder?.isRecording == true {
+            audioRecorder?.stop()
+        } else {
+            audioPlayer?.stop()
+        }
+        
+        if audioRecorder?.isRecording == false {
+            stopButton.isEnabled = true
+            recordButton.isEnabled = false
+            
+            do {
+                try audioPlayer = AVAudioPlayer(contentsOf:
+                    (audioRecorder?.url)!)
+                audioPlayer!.delegate = self
+                audioPlayer!.prepareToPlay()
+                audioPlayer!.play()
+            } catch let error as NSError {
+                print("audioPlayer error: \(error.localizedDescription)")
+            }
+        }
+        
+        stopButton.isEnabled = false
+        recordButton.isEnabled = true
+        
+    }
+    
+    @IBAction func recordSound(_ sender: Any) {
+        
+        if audioRecorder?.isRecording == false {
+            stopButton.isEnabled = true
+            audioRecorder?.record()
+        }
+        
+        
+    }
+    
+    
+    
 }
 
